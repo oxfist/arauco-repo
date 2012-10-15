6bcfb46697085c54ca5bec351827831b9fed2093
Andrés Caro <andres.caro.q@gmail.com>
2012-10-15 20:39:24 +0000

    Fixed yuicompressor.jar issue (used 2.4.2 version) and now YUI Compressor is really working. Also FOSJsRoutingBundle is now installed with Composer and working nicely.

--- src/Andres/Bundle/TestBundle/Controller/DefaultController.php
+++ src/Andres/Bundle/TestBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        if ( $this->container->get('request')->isXmlHttpRequest() )
+            return new Response( "<b>Content loaded with AJAX</b>" );
         return $this->render('AndresTestBundle:Default:index.html.twig');
     }
 }
