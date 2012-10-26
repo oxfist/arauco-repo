9c5c65bc84821ca5351dd7282f25cfeb680ea544
Andrés Caro <andres.caro.q@gmail.com>
2012-10-26 09:46:01 +0000

    Doctrine set up to create table

--- src/Andres/Bundle/TestBundle/Controller/DefaultController.php
+++ src/Andres/Bundle/TestBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Andres\Bundle\TestBundle\Controller;
 
+use Andres\Bundle\TestBundle\Entity\Menu;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,5 +16,19 @@ class DefaultController extends Controller
     public function contentAction()
     {
         return $this->render('AndresTestBundle:Default:content.html.twig');
+    }
+
+    public function createAction()
+    {
+        $menu = new Menu();
+        $menu->setNombre('Tallarines');
+        $menu->setPrecio('1000.5');
+        $menu->setDescripcion('Tallarines muy ricos');
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($menu);
+        $em->flush();
+
+        return new Response('Creado menu con id '. $menu->getId());
     }
 }
