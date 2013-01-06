626f03fe90807a791f153b8adf2f02881b77590d
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-06 19:39:10 -0300

    Ready Export CSV with example (Stock)

--- src/Arauco/CSVBundle/Controller/StockController.php
+++ src/Arauco/CSVBundle/Controller/StockController.php
@@ -30,10 +30,34 @@ class StockController extends Controller
                 FROM AraucoCSVBundle:Stock S
                 WHERE S.Status <> 'A'"
                 );
-        
+
         $StockSinAsignar = $query->getResult();
 
         return array('total' => $total,
                      'stocksa' => $StockSinAsignar);
     }
+
+    /**
+     * @Route("/stockcsv", name="arauco_stock_csv")
+     */
+    public function csvAction() {
+
+    	$em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                "SELECT S.Lote, S.Material, S.Desc_Mat
+                FROM AraucoCSVBundle:Stock S
+                WHERE S.Status <> 'A'"
+                );
+        $data = $query->getResult();
+
+        $filename = "Stock_CSV".date("Y_m_d_His").".csv";
+
+        $response = $this->render('AraucoBaseBundle:Stock:stockCsv.html.twig', array('data' => $data));
+
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename='.$filename);
+
+        return $response;
+    }
+
 }
