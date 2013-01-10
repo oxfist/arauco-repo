c26bd412ea29adad5724f51b37c2016502904098
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-09 23:33:51 -0300

    SQL to DQL with Sh1ken's Query (Working on master e_e)

--- src/Arauco/CSVBundle/Controller/PedidosController.php
+++ src/Arauco/CSVBundle/Controller/PedidosController.php
@@ -14,7 +14,12 @@ class PedidosController extends Controller
      */
     public function importAction ()
     {
-        return array();
-    }
+    	$em = $this->getDoctrine()->getManager();
 
+        $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
+
+        $Entregas = $query->getResult();
+
+        return array('Entregas' => $Entregas);
+    }
 }
