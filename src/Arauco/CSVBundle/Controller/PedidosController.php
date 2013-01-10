4f5ff24530642d917ccc6728f4f094076f48b6e5
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-10 13:41:03 -0300

    CSV Ready with graph

--- src/Arauco/CSVBundle/Controller/PedidosController.php
+++ src/Arauco/CSVBundle/Controller/PedidosController.php
@@ -41,12 +41,12 @@ class PedidosController extends Controller
     }
 
     /**
-     * @Route("/pedido/{id}", name="arauco_pedido_extend")
+     * @Route("/pedido/{week}", name="arauco_pedido_extend")
      * @Template("AraucoBaseBundle:Pedido:extend.html.twig")
      */
-    public function extendAction ()
+    public function extendAction ($week)
     {
-        $cantOfWeeks = 2;
+        $cantOfWeeks = $week;
 
         $day = date('d', strtotime("+". $cantOfWeeks." week"));
         $month = date('m', strtotime("+". $cantOfWeeks." week"));
@@ -58,13 +58,51 @@ class PedidosController extends Controller
         $start_week = date('Ymd', mktime(0,0,0,$month, $sunday+1, $year));
         $end_week   = date('Ymd', mktime(0,0,0,$month, $sunday+7, $year)); 
 
+        $sWeek = date('d/m/Y', mktime(0,0,0,$month, $sunday+1, $year));
+        $eWeek   = date('d/m/Y', mktime(0,0,0,$month, $sunday+7, $year)); 
+
         $em = $this->getDoctrine()->getManager();
 
         $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.Eta >=".$start_week."AND P.Eta <=".$end_week." AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
 
         $Entregas = $query->getResult();
-        $cantEntregas = count($Entregas);
 
-        return array('Entregas' => $Entregas, 'cantEntregas' => $cantEntregas);
+        return array('Entregas' => $Entregas, 'sWeek' => $sWeek, 'eWeek' => $eWeek, 'week' => $week);
+    }
+
+    /**
+     * @Route("/pedido/{week}/csv", name="arauco_pedido_extendcsv")
+     */
+    public function extendcsvAction ($week)
+    {
+        $cantOfWeeks = $week;
+
+        $day = date('d', strtotime("+". $cantOfWeeks." week"));
+        $month = date('m', strtotime("+". $cantOfWeeks." week"));
+        $year = date('Y', strtotime("+". $cantOfWeeks." week"));
+
+        $weekday = date('w', mktime(0,0,0,$month, $day, $year));
+        $sunday  = $day - $weekday;
+
+        $start_week = date('Ymd', mktime(0,0,0,$month, $sunday+1, $year));
+        $end_week   = date('Ymd', mktime(0,0,0,$month, $sunday+7, $year)); 
+
+        $sWeek = date('d/m/Y', mktime(0,0,0,$month, $sunday+1, $year));
+        $eWeek   = date('d/m/Y', mktime(0,0,0,$month, $sunday+7, $year)); 
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.Eta >=".$start_week."AND P.Eta <=".$end_week." AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
+
+        $data = $query->getResult();
+
+        $filename = "Pedidos_".$start_week."_".$end_week."_".date("Y_m_d_His").".csv";
+
+        $response = $this->render('AraucoBaseBundle:Pedido:pedidosCsv.html.twig', array('data' => $data));
+
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename='.$filename);
+
+        return $response;
     }
 }
