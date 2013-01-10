3a268ec66c9eac00794d7bbc14a28e88b99af07a
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-10 12:42:58 -0300

    CHECKPOINT GRAPH

--- src/Arauco/CSVBundle/Controller/PedidosController.php
+++ src/Arauco/CSVBundle/Controller/PedidosController.php
@@ -5,6 +5,8 @@ namespace Arauco\CSVBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Arauco\CSVBundle\Entity\Stock;
+use Arauco\CSVBundle\Entity\Pedidos;
 
 class PedidosController extends Controller
 {
@@ -14,12 +16,55 @@ class PedidosController extends Controller
      */
     public function importAction ()
     {
-    	$em = $this->getDoctrine()->getManager();
+        for ($i = 0; $i < 4; $i++) {
+            $cantOfWeeks = $i;
 
-        $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
+            $day = date('d', strtotime("+". $cantOfWeeks." week"));
+            $month = date('m', strtotime("+". $cantOfWeeks." week"));
+            $year = date('Y', strtotime("+". $cantOfWeeks." week"));
+
+            $weekday = date('w', mktime(0,0,0,$month, $day, $year));
+            $sunday  = $day - $weekday;
+
+            $start_week = date('Ymd', mktime(0,0,0,$month, $sunday+1, $year));
+            $end_week   = date('Ymd', mktime(0,0,0,$month, $sunday+7, $year)); 
+
+            $em = $this->getDoctrine()->getManager();
+
+            $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.Eta >=".$start_week."AND P.Eta <=".$end_week." AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
+
+            $Entregas = $query->getResult();
+            $cantEntregas[$i] = count($Entregas);
+        }
+
+        return array('cantEntregas' => $cantEntregas);
+    }
+
+    /**
+     * @Route("/pedido/{id}", name="arauco_pedido_extend")
+     * @Template("AraucoBaseBundle:Pedido:extend.html.twig")
+     */
+    public function extendAction ()
+    {
+        $cantOfWeeks = 2;
+
+        $day = date('d', strtotime("+". $cantOfWeeks." week"));
+        $month = date('m', strtotime("+". $cantOfWeeks." week"));
+        $year = date('Y', strtotime("+". $cantOfWeeks." week"));
+
+        $weekday = date('w', mktime(0,0,0,$month, $day, $year));
+        $sunday  = $day - $weekday;
+
+        $start_week = date('Ymd', mktime(0,0,0,$month, $sunday+1, $year));
+        $end_week   = date('Ymd', mktime(0,0,0,$month, $sunday+7, $year)); 
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery("SELECT P.DocEntrega, P.PosPedido, S.Material, S.Desc_Mat, P.VolPedido, SUM( S.M3 ) as M3 FROM AraucoCSVBundle:Pedidos P, AraucoCSVBundle:Stock S WHERE P.DocEntrega = S.Nro_Entrega AND P.PosPedido = S.Pos_Entrega AND P.Eta >=".$start_week."AND P.Eta <=".$end_week." AND P.StatusMovimientodeMcia = 'A' GROUP BY P.DocEntrega, P.PosPedido ORDER BY P.DocEntrega");
 
         $Entregas = $query->getResult();
+        $cantEntregas = count($Entregas);
 
-        return array('Entregas' => $Entregas);
+        return array('Entregas' => $Entregas, 'cantEntregas' => $cantEntregas);
     }
 }
