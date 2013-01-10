e85acb3a08b0bfd704006c2410a0de1ea3c7a4f0
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-10 14:14:51 -0300

    Stock by ID. Fixed views

--- src/Arauco/CSVBundle/Controller/StockController.php
+++ src/Arauco/CSVBundle/Controller/StockController.php
@@ -38,6 +38,24 @@ class StockController extends Controller
     }
 
     /**
+     * @Route("/stock/{id}", name="arauco_stock_extend")
+     * @Template("AraucoBaseBundle:Stock:extend.html.twig")
+     */
+    public function importextendAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                "SELECT S.Lote, S.Desc_Mat, S.M3
+                FROM AraucoCSVBundle:Stock S
+                WHERE S.Material = ".$id." AND S.Status <> 'A'"
+                );
+
+        $StockSinAsignar = $query->getResult();
+
+        return array('stocksa' => $StockSinAsignar, 'id' => $id);
+    }
+
+    /**
      * @Route("/stockcsv", name="arauco_stock_csv")
      */
     public function csvAction() {
@@ -53,6 +71,29 @@ class StockController extends Controller
         $filename = "Stock_CSV".date("Y_m_d_His").".csv";
 
         $response = $this->render('AraucoBaseBundle:Stock:stockCsv.html.twig', array('data' => $data));
+
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename='.$filename);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/stockcsv/{id}", name="arauco_stock_csvextend")
+     */
+    public function csvextendAction($id) {
+
+    	$em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+                "SELECT S.Lote, S.M3
+                FROM AraucoCSVBundle:Stock S
+                WHERE S.Material = ".$id." AND S.Status <> 'A'"
+                );
+        $data = $query->getResult();
+
+        $filename = "Stock_".$id."_".date("Y_m_d_His").".csv";
+
+        $response = $this->render('AraucoBaseBundle:Stock:stockextendCsv.html.twig', array('data' => $data));
 
         $response->headers->set('Content-Type', 'text/csv');
         $response->headers->set('Content-Disposition', 'attachment; filename='.$filename);
