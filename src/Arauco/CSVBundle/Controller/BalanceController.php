c613d516eb8104ba6742536d2838b5aa48629774
Felipe Larenas <felipe.larenas@usach.cl>
2013-01-11 06:40:35 -0300

    Fixed position graph

--- src/Arauco/CSVBundle/Controller/BalanceController.php
+++ src/Arauco/CSVBundle/Controller/BalanceController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 ini_set('memory_limit', '-1');
 
 class BalanceController extends Controller
-{    
+{
     /**
      * @Route("/balance", name="arauco_balance_index")
      * @Template("AraucoBaseBundle:Balance:index.html.twig")
@@ -21,9 +21,9 @@ class BalanceController extends Controller
                 ->findAllTotalStock();
         $pedidos = $em->getRepository('AraucoCSVBundle:Pedidos')
                 ->findAllTotalPedidos();
-        
+
         $total = array();
-        
+
         foreach ($stock as $item) {
             $material = $item['Material'];
             $descMaterial = $item['Desc_Mat'];
@@ -33,7 +33,7 @@ class BalanceController extends Controller
             $totalBalance = 0;
             $totalStock = 0;
             $totalPedidos = 0;
-            
+
             foreach ($pedidos as $pedido) {
                 if ( $pedido['Material'] == $material ) {
                     $totalPedidosPorMaterial = $pedido['Suma'];
@@ -42,24 +42,24 @@ class BalanceController extends Controller
                     break;
                 }
             }
-            
+
             if ( !$totalPedidosPorMaterial ) {
                 $totalPedidosPorMaterial = 0;
                 $balance = $stock;
             }
-            
+
             array_push( $total, array( $material, $descMaterial,
                 $stock, $totalPedidosPorMaterial, $balance ) );
         }
-        
+
         foreach ( $total as $item ) {
             $totalStock = $totalStock + $item[2];
             $totalPedidos = $totalPedidos + $item[3];
             $totalBalance = $totalBalance + $item[4];
         }
-        
+
         usort( $total, array( $this, "Compare" ) );
-        
+
         return array(
             'total' => $total,
             'totalBalance' => $totalBalance,
@@ -67,7 +67,7 @@ class BalanceController extends Controller
             'totalPedidos' => $totalPedidos
             );
     }
-    
+
     private static function Compare( $a, $b )
     {
         return $a[4] < $b[4];
